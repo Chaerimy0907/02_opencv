0cbc8211852f9d98aef0a4f88c02cbcc1336fa83
Chaerimy0907 <qkrcofus1026@daum.net>
2025-07-29 10:23:35 +0900

    기본값

---
+++ src/color.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+# 기본값
+img = cv2.imread('../img/like_lenna.png')
+
+# shape
+print("default", img.shape)
+
+cv2.imshow('img',img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
