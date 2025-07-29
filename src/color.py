9fdaf80c97abc5f6c22271c63165d7a99051ac7e
Chaerimy0907 <qkrcofus1026@daum.net>
2025-07-29 10:31:58 +0900

    BGRA 추가

--- src/color.py
+++ src/color.py
@@ -7,11 +7,15 @@ img = cv2.imread('../img/like_lenna.png')
 # bgr
 bgr = cv2.imread('../img/like_lenna.png',cv2.IMREAD_COLOR)
 
-# shape
-print("default", img.shape, "color", bgr.shape)
+# bgra
+bgra = cv2.imread('../img/like_lenna.png',cv2.IMREAD_UNCHANGED)
 
-cv2.imshow('img',img)
-cv2.imshow('bgr',bgr)
+# shape
+print("default", img.shape, "color", bgr.shape, "unchanged", bgra.shape)
+
+cv2.imshow('img', img)
+cv2.imshow('bgr', bgr)
+cv2.imshow('bgra', bgra[:,:,3])
 
 cv2.waitKey(0)
 cv2.destroyAllWindows
